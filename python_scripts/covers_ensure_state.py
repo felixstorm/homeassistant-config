a89fe691905b16f2c166624024241447fd2fdc2d
Felix Storm <felix.storm@glueckkanja.com>
2018-07-18 10:57:42 +0200

    covers_coverprot.yaml

--- python_scripts/covers_ensure_state.py
+++ python_scripts/covers_ensure_state.py
@@ -15,11 +15,17 @@ def ensure_state(hass, logger, target_state, entity_id):
     if current_state is None:
         logger.error('Error getting current state for entity_id \'{}\' - aborting.'.format(entity_id))
         return
+    message_prefix = '*** ENSURE_STATE for cover {} ({}): '.format(current_state.attributes.get('friendly_name'), entity_id)
+
+    if 'ubisys_j1_5502_0000243d' in entity_id or 'ubisys_j1_5502_00000dba' in entity_id:    # Wintergarten Markise, Dachfenster groß
+        coverprot_is_active = hass.states.get('input_boolean.coverprot_wind_active').state != 'off'
+        if coverprot_is_active:
+            logger.info(message_prefix + 'Cover protection is active, aborting.')
+            return
 
     target_lift = None
     target_tilt = None
     ignore_fully_closed = False
-    maximum_wind_speed = None
 
     if target_state == 'morning':
         target_lift = 100
@@ -39,19 +45,18 @@ def ensure_state(hass, logger, target_state, entity_id):
     else:
         raise KeyError('Invalid face. Expected morning, nighttime, sunscreen or no-sunscreen, got: {}'.format(target_state))
 
-    friendly_name = current_state.attributes.get('friendly_name') 
     current_lift = current_state.attributes.get('current_position')
     current_tilt = current_state.attributes.get('current_tilt_position')
 
     set_lift = (current_lift is not 'unknown') and (target_lift is not None) and (not ignore_fully_closed or current_lift != 0) and (abs(current_lift - target_lift) > 3)
     set_tilt = (current_tilt is not 'unknown') and (target_tilt is not None) and (not ignore_fully_closed or current_lift != 0) and (abs(current_tilt - target_tilt) > 3)
     
-    message = '*** ENSURE_STATE for cover {} ({}): target_lift = {}, target_tilt = {}, ignore_fully_closed = {}, current_lift = {}, current_tilt = {}, set_lift = {}, set_tilt = {}'.format(
-        friendly_name, entity_id, target_lift, target_tilt, ignore_fully_closed, current_lift, current_tilt, set_lift, set_tilt)
+    message = 'target_lift = {}, target_tilt = {}, ignore_fully_closed = {}, current_lift = {}, current_tilt = {}, set_lift = {}, set_tilt = {}'.format(
+        target_lift, target_tilt, ignore_fully_closed, current_lift, current_tilt, set_lift, set_tilt)
     if set_lift or set_tilt:
-        logger.info(message)
+        logger.info(message_prefix + message)
     else:
-        logger.debug(message)
+        logger.debug(message_prefix + message)
 
     if set_tilt:
         hass.services.call('cover', 'set_cover_tilt_position', {'entity_id': entity_id, 'tilt_position': target_tilt}, False)
