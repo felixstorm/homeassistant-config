ba51c2da60e6ed8986225c7cc8c564cdcdfa99fd
Felix Storm <felix.storm@glueckkanja.com>
2018-08-05 13:52:19 +0200

    covers_ensure_state.py: handle sun protection internally

--- python_scripts/covers_ensure_state.py
+++ python_scripts/covers_ensure_state.py
@@ -1,21 +1,21 @@
 # Sample Service Data
-# { "face": "east", "target_state": "morning" }
+# { "situation": "morning" }
 
-face = data.get('face', 'all')
-if face not in ['all', 'east', 'south', 'west']:
-    raise KeyError('Invalid face. Expected all (default), east, south or west, got: {}'.format(face))
-target_state = data.get('target_state')
-if target_state not in ['morning', 'nighttime', 'sunscreen', 'no-sunscreen']:
-    raise KeyError('Invalid target_state. Expected morning, nighttime, sunscreen or no-sunscreen, got: {}'.format(target_state))
+situation = data.get('situation', 'daytime')
+if situation not in ['morning', 'daytime', 'nighttime']:
+    raise KeyError('Invalid situation. Expected morning, daytime or nighttime, got: {}'.format(situation))
 
 
-def ensure_state(hass, logger, target_state, entity_id):
+
+def ensure_state(hass, logger, situation, sunprot_is_active, entity_id):
+
 
     current_state = hass.states.get(entity_id)
     if current_state is None:
         logger.error('Error getting current state for entity_id \'{}\' - aborting.'.format(entity_id))
         return
     message_prefix = '*** ENSURE_STATE for cover {} ({}): '.format(current_state.attributes.get('friendly_name'), entity_id)
+
 
     if 'ubisys_j1_5502_0000243d' in entity_id or 'ubisys_j1_5502_00000dba' in entity_id:    # Wintergarten Markise, Dachfenster groß
         coverprot_is_active = hass.states.get('input_boolean.coverprot_wind_active').state != 'off'
@@ -29,32 +29,33 @@ def ensure_state(hass, logger, target_state, entity_id):
             logger.debug(message_prefix + 'Cover Terrassentuer is not supposed to be controlled automatically, aborting.')
             return
 
+
     target_lift = None
     target_tilt = None
     ignore_fully_closed = False
 
-    if target_state == 'morning':
-        target_lift = 100
-    elif target_state == 'nighttime':
+    if situation in ['morning', 'daytime']:
+        ignore_fully_closed = situation != 'morning'
+        if sunprot_is_active:
+            target_lift = 10
+            target_tilt = 50
+            if 'ubisys_j1_5502_00002c8c' in entity_id:  # Küche Terrassentür
+                target_lift = 60            # Platz (für die Kinder) zum Durchlaufen
+            if 'ubisys_j1_5502_0000243d' in entity_id:  # Wintergarten Markise
+                target_lift = 0             # komplett ausfahren
+        else:
+            target_lift = 100
+            if 'ubisys_j1_5502_0000243d' in entity_id:  # Wintergarten Markise
+                ignore_fully_closed = False # Markise wird auch zum Sonnenschutz komplett ausgefahren und dient nicht zur Verdunkelung
+
+    elif situation == 'nighttime':
         target_lift = 0
-        if 'ubisys_j1_5502_0000243d' in entity_id:    # Wintergarten Markise
+        if 'ubisys_j1_5502_0000243d' in entity_id:      # Wintergarten Markise
             target_lift = 100   # Markisen nachts einfahren, nicht ausfahren
-    elif target_state == 'sunscreen':
-        target_lift = 10
-        target_tilt = 50
-        ignore_fully_closed = True
-        if 'ubisys_j1_5502_00002c8c' in entity_id:  # Küche Terrassentür
-            target_lift = 60    # Platz (für die Kinder) zum Durchlaufen
-        if 'ubisys_j1_5502_0000243d' in entity_id:  # Wintergarten Markise
-            target_lift = 0             # komplett ausfahren
-            ignore_fully_closed = False # Markise dient nicht zur Verdunkelung
-    elif target_state == 'no-sunscreen':
-        target_lift = 100
-        ignore_fully_closed = True
-        if 'ubisys_j1_5502_0000243d' in entity_id:  # Wintergarten Markise
-            ignore_fully_closed = False # Markise dient nicht zur Verdunkelung
+
     else:
-        raise KeyError('Invalid face. Expected morning, nighttime, sunscreen or no-sunscreen, got: {}'.format(target_state))
+        raise KeyError('Invalid situation. Expected morning, daytime or nighttime, got: {}'.format(situation))
+
 
     current_lift = current_state.attributes.get('current_position')
     current_tilt = current_state.attributes.get('current_tilt_position')
@@ -83,24 +84,27 @@ def ensure_state(hass, logger, target_state, entity_id):
         time.sleep(5)
 
 
-if face in ['east', 'all']:
-    ensure_state(hass, logger, target_state, 'cover.ubisys_j1_5502_00002415_1') # Wohnzimmer Sitzfenster
-    ensure_state(hass, logger, target_state, 'cover.ubisys_j1_5502_000024db_1') # Wohnzimmer Terrassentür
-    ensure_state(hass, logger, target_state, 'cover.ubisys_j1_5502_00000df3_1') # Florian
-    ensure_state(hass, logger, target_state, 'cover.ubisys_j1_5502_00000ded_1') # Jonathan links
-    ensure_state(hass, logger, target_state, 'cover.ubisys_j1_5502_00002cc0_1') # Studio DG links
-    ensure_state(hass, logger, target_state, 'cover.ubisys_j1_5502_0000240d_1') # Studio DG rechts
 
-if face in ['south', 'all']:
-    ensure_state(hass, logger, target_state, 'cover.ubisys_j1_5502_0000243d_1') # Wintergarten Markise
-    ensure_state(hass, logger, target_state, 'cover.ubisys_j1_5502_00002c8c_1') # Küche Terrassentür
-    ensure_state(hass, logger, target_state, 'cover.ubisys_j1_5502_000024aa_1') # Jonathan rechts
-    ensure_state(hass, logger, target_state, 'cover.ubisys_j1_5502_00000dba_1') # Dachfenster groß
+sunprot_eastface_is_active = hass.states.get('input_boolean.sunprot_eastface_active').state == 'on'
+sunprot_southface_is_active = hass.states.get('input_boolean.sunprot_southface_active').state == 'on'
+sunprot_westface_is_active = hass.states.get('input_boolean.sunprot_westface_active').state == 'on'
+logger.debug('*** ENSURE_STATE for covers: sunprot_eastface_is_active={}, sunprot_southface_is_active={}, sunprot_westface_is_active={}'.format(sunprot_eastface_is_active, sunprot_southface_is_active, sunprot_westface_is_active))
 
-if face in ['west', 'all']:
-    ensure_state(hass, logger, target_state, 'cover.ubisys_j1_5502_00001110_1') # Windfang
-    ensure_state(hass, logger, target_state, 'cover.ubisys_j1_5502_00000dc6_1') # Küche Fenster
-    ensure_state(hass, logger, target_state, 'cover.ubisys_j1_5502_0000255e_1') # Schlafzimmer
-    ensure_state(hass, logger, target_state, 'cover.ubisys_j1_5502_00000dec_1') # Bad OG
-    ensure_state(hass, logger, target_state, 'cover.ubisys_j1_5502_0000243e_1') # Flur OG
-    ensure_state(hass, logger, target_state, 'cover.ubisys_j1_5502_00002cec_1') # Bad DG
+ensure_state(hass, logger, situation, sunprot_eastface_is_active, 'cover.ubisys_j1_5502_00002415_1') # Wohnzimmer Sitzfenster
+ensure_state(hass, logger, situation, sunprot_eastface_is_active, 'cover.ubisys_j1_5502_000024db_1') # Wohnzimmer Terrassentür
+ensure_state(hass, logger, situation, sunprot_eastface_is_active or sunprot_southface_is_active or sunprot_westface_is_active, 'cover.ubisys_j1_5502_0000243d_1') # Wintergarten Markise
+ensure_state(hass, logger, situation, sunprot_southface_is_active, 'cover.ubisys_j1_5502_00002c8c_1') # Küche Terrassentür
+ensure_state(hass, logger, situation, sunprot_westface_is_active, 'cover.ubisys_j1_5502_00001110_1') # Windfang
+ensure_state(hass, logger, situation, sunprot_westface_is_active, 'cover.ubisys_j1_5502_00000dc6_1') # Küche Fenster
+
+ensure_state(hass, logger, situation, sunprot_eastface_is_active, 'cover.ubisys_j1_5502_00000df3_1') # Florian
+ensure_state(hass, logger, situation, sunprot_eastface_is_active, 'cover.ubisys_j1_5502_00000ded_1') # Jonathan links
+ensure_state(hass, logger, situation, sunprot_southface_is_active, 'cover.ubisys_j1_5502_000024aa_1') # Jonathan rechts
+ensure_state(hass, logger, situation, sunprot_westface_is_active, 'cover.ubisys_j1_5502_0000255e_1') # Schlafzimmer
+ensure_state(hass, logger, situation, sunprot_westface_is_active, 'cover.ubisys_j1_5502_00000dec_1') # Bad OG
+ensure_state(hass, logger, situation, sunprot_westface_is_active, 'cover.ubisys_j1_5502_0000243e_1') # Flur OG
+
+ensure_state(hass, logger, situation, sunprot_eastface_is_active, 'cover.ubisys_j1_5502_00002cc0_1') # Studio DG links
+ensure_state(hass, logger, situation, sunprot_eastface_is_active, 'cover.ubisys_j1_5502_0000240d_1') # Studio DG rechts
+ensure_state(hass, logger, situation, sunprot_southface_is_active or sunprot_westface_is_active, 'cover.ubisys_j1_5502_00000dba_1') # Dachfenster groß
+ensure_state(hass, logger, situation, sunprot_westface_is_active, 'cover.ubisys_j1_5502_00002cec_1') # Bad DG
