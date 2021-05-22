138c921ab1b29f462fc22b222a10b7c2fb8539b6
Felix Storm <felix.storm@glueckkanja.com>
2021-05-22 11:53:12 +0200

    covers_ensure_state.py

--- python_scripts/covers_ensure_state.py
+++ python_scripts/covers_ensure_state.py
@@ -1,8 +1,8 @@
 # Sample Service Data
-# { "situation": "morning" }
+# { "situation": "covers_up" }
 
-situation = data.get('situation', 'daytime')
-valid_situations = ['morning', 'morning-simulation', 'daytime', 'evening', 'nighttime', 'nighttime-simulation']
+situation = data.get('situation', 'default')
+valid_situations = ['covers_up', 'covers_up-simulation', 'default', 'evening', 'covers_down', 'covers_down-simulation']
 if situation not in valid_situations:
     raise KeyError("Invalid parameter 'situation'. Expected {}, got '{}'".format(valid_situations, situation))
 if situation.endswith('-simulation'):
@@ -20,14 +20,16 @@ coverprot_rain_is_active = hass.states.get('input_boolean.coverprot_rain_active'
 coverprot_freeze_is_active = hass.states.get('input_boolean.coverprot_freeze_active').state != 'off'
 terrassentuer_is_closed = hass.states.get('binary_sensor.f1_senwin_terrassentur_contact').state == 'off'
 include_kuechentuer_in_automations = hass.states.get('input_boolean.covers_include_kuechentuer_in_automations').state == 'on'
+dachfenster_gross_innen_is_closed = hass.states.get('cover.f3_cov_dachfenster_gross_cover').attributes.get('current_position') == 0
 dg_markisen_needed_at_night = hass.states.get('binary_sensor.dg_markisen_needed_at_night').state == 'on'
 
 
 
-def ensure_state(sunprot_is_active, entity_id):
+def ensure_state(entity_id, sunprot_is_active):
 
     global hass, logger, situation, is_simulation
-    global coverprot_wind_is_active, coverprot_rain_is_active, coverprot_freeze_is_active, terrassentuer_is_closed, include_kuechentuer_in_automations, dg_markisen_needed_at_night
+    global coverprot_wind_is_active, coverprot_rain_is_active, coverprot_freeze_is_active, terrassentuer_is_closed, \
+        include_kuechentuer_in_automations, dachfenster_gross_innen_is_closed, dg_markisen_needed_at_night
 
     current_state = hass.states.get(entity_id)
     if current_state is None:
@@ -44,10 +46,10 @@ def ensure_state(sunprot_is_active, entity_id):
     target_tilt = None
     skip_if_closed_completely = False
 
-    if situation in ['morning', 'daytime']:
+    if situation in ['covers_up', 'default']:
         # tagsüber (manuell) komplett geschlossene Raffstore in Ruhe lassen (Mittagsschlaf, Blendschutz etc.) und
         # Markisen auch morgens (falls sie dann schon manuell komplett ausgefahren worden sind)
-        skip_if_closed_completely = (situation != 'morning') or ('markise' in entity_id)
+        skip_if_closed_completely = (situation != 'covers_up') or ('markise' in entity_id)
         if sunprot_is_active:
             target_lift = 5
             target_tilt = 50
@@ -57,15 +59,15 @@ def ensure_state(sunprot_is_active, entity_id):
         else:
             target_lift = 100
 
-    elif situation in ['evening', 'nighttime']:
+    elif situation in ['evening', 'covers_down']:
         # Markisen abends & nachts einfahren (außer Dachfenster wg. zusätzlicher Verdunkelung)
         if 'markise' in entity_id:
             target_lift = 100
         # den Rest nachts ausfahren (abends ignorieren)
-        elif situation == 'nighttime':
+        elif situation == 'covers_down':
             if is_simulation == False:
                 if dg_markisen_needed_at_night == False and ('dachfenster_gross_aussen_cover' in entity_id or 'f3_cov_bad_cover' in entity_id):
-                    logger.debug(message_prefix + 'Markisen DG are not needed at this time of the year, aborting.')
+                    logger.info(message_prefix + 'Markisen DG are not needed at this time of the year, aborting.')
                     return
                 target_lift = 0
             else:
@@ -94,6 +96,12 @@ def ensure_state(sunprot_is_active, entity_id):
     # but current state is not known
     current_lift = current_state.attributes.get('current_position')
     current_tilt = current_state.attributes.get('current_tilt_position')
+
+    if situation == 'default' and ('dachfenster_gross_aussen_cover' in entity_id or 'f3_cov_bad_cover' in entity_id) and dachfenster_gross_innen_is_closed and dg_markisen_needed_at_night and current_lift != 0:
+        # Wenn die Innenverdunkelung des großen Dachfensters geschlossen ist, dann auch die DG SW-Markisen ausfahren, damit sie z.B. nach
+        # Aufhebung von Windalarm nachts automatisch wieder herunter gefahren werden und nicht morgens noch offen sind (und Licht hereinlassen)
+        logger.info(message_prefix + 'Also closing this as Dachfenster innen is closed.')
+        target_lift = 0
 
     set_lift = (
         target_lift is not None
@@ -143,32 +151,37 @@ sunprot_southface_is_active = hass.states.get('input_boolean.sunprot_southface_a
 sunprot_westface_is_active = hass.states.get('input_boolean.sunprot_westface_active').state == 'on'
 sunprot_wiga_is_active = hass.states.get('input_boolean.sunprot_wiga_active').state == 'on'
 use_terrasse_markise = hass.states.get('input_boolean.covers_use_terrasse_markise_for_sunprotection').state == 'on'
+
 logger.debug('*** ENSURE_STATE for covers: sunprot_eastface_is_active={}, sunprot_southface_is_active={}, sunprot_westface_is_active={}, use_terrasse_markise={}'
     .format(sunprot_eastface_is_active, sunprot_southface_is_active, sunprot_westface_is_active, use_terrasse_markise))
+logger.debug('*** ENSURE_STATE for covers: coverprot_wind_is_active={}, coverprot_rain_is_active={}, coverprot_freeze_is_active={}, terrassentuer_is_closed={}'
+    .format(coverprot_wind_is_active, coverprot_rain_is_active, coverprot_freeze_is_active, terrassentuer_is_closed))
+logger.debug('*** ENSURE_STATE for covers: include_kuechentuer_in_automations={}, dachfenster_gross_innen_is_closed={}, dg_markisen_needed_at_night={}'
+    .format(include_kuechentuer_in_automations, dachfenster_gross_innen_is_closed, dg_markisen_needed_at_night))
 
 if level is None or 'eg' in level:
-    ensure_state(sunprot_eastface_is_active, 'cover.f1_cov_wohnzimmer_sitzfenster_cover')
+    ensure_state('cover.f1_cov_wohnzimmer_sitzfenster_cover',   sunprot_eastface_is_active)
     # Für den Sonnenschutz werden entweder die Markise oder die Raffstore benötigt, aber nicht beides gleichzeitig.
-    ensure_state(sunprot_eastface_is_active and not use_terrasse_markise, 'cover.f1_cov_wohnzimmer_terrassentur_cover')
-    ensure_state(sunprot_eastface_is_active and use_terrasse_markise, 'cover.f1_cov_terrasse_markise_cover')
-    ensure_state(sunprot_wiga_is_active, 'cover.f1_cov_wintergarten_markise_cover')
-    ensure_state(sunprot_southface_is_active, 'cover.f1_cov_kuche_terrassentur_cover')
-    ensure_state(sunprot_westface_is_active, 'cover.f1_cov_windfang_cover')
-    ensure_state(sunprot_westface_is_active, 'cover.f1_cov_kuche_fenster_cover')
+    ensure_state('cover.f1_cov_wohnzimmer_terrassentur_cover',  sunprot_eastface_is_active and not use_terrasse_markise)
+    ensure_state('cover.f1_cov_terrasse_markise_cover',         sunprot_eastface_is_active and use_terrasse_markise)
+    ensure_state('cover.f1_cov_wintergarten_markise_cover',     sunprot_wiga_is_active)
+    ensure_state('cover.f1_cov_kuche_terrassentur_cover',       sunprot_southface_is_active)
+    ensure_state('cover.f1_cov_windfang_cover',                 sunprot_westface_is_active)
+    ensure_state('cover.f1_cov_kuche_fenster_cover',            sunprot_westface_is_active)
 
 if level is None or 'og' in level:
-    ensure_state(sunprot_eastface_is_active, 'cover.f2_cov_florian_cover')
-    ensure_state(sunprot_eastface_is_active, 'cover.f2_cov_jonathan_links_cover')
-    ensure_state(sunprot_southface_is_active, 'cover.f2_cov_jonathan_rechts_cover')
-    ensure_state(sunprot_westface_is_active, 'cover.f2_cov_schlafzimmer_cover')
-    ensure_state(sunprot_westface_is_active, 'cover.f2_cov_bad_cover')
-    ensure_state(sunprot_westface_is_active, 'cover.f2_cov_flur_cover')
+    ensure_state('cover.f2_cov_florian_cover',                  sunprot_eastface_is_active)
+    ensure_state('cover.f2_cov_jonathan_links_cover',           sunprot_eastface_is_active)
+    ensure_state('cover.f2_cov_jonathan_rechts_cover',          sunprot_southface_is_active)
+    ensure_state('cover.f2_cov_schlafzimmer_cover',             sunprot_westface_is_active)
+    ensure_state('cover.f2_cov_bad_cover',                      sunprot_westface_is_active)
+    ensure_state('cover.f2_cov_flur_cover',                     sunprot_westface_is_active)
 
 if level is None or 'dg' in level:
-    ensure_state(sunprot_eastface_is_active, 'cover.f3_cov_studio_links_cover')
-    ensure_state(sunprot_eastface_is_active, 'cover.f3_cov_studio_rechts_cover')
+    ensure_state('cover.f3_cov_studio_links_cover',             sunprot_eastface_is_active)
+    ensure_state('cover.f3_cov_studio_rechts_cover',            sunprot_eastface_is_active)
     # Die innenliegende Verschattung wird nicht für den Sonnenschutz benutzt, daher ist `sunprot_is_active` immer False.
-    ensure_state(False, 'cover.f3_cov_dachfenster_gross_cover')
-    ensure_state(sunprot_southface_is_active or sunprot_westface_is_active, 'cover.f3_cov_dachfenster_gross_aussen_cover')
-    # # Im Bad die Beschattung herunterfahren, sobald irgendein Sonnenschutz aktiv ist, da es sonst dort sehr warm wird.
-    ensure_state(sunprot_southface_is_active or sunprot_westface_is_active, 'cover.f3_cov_bad_cover')
+    ensure_state('cover.f3_cov_dachfenster_gross_cover',        False)
+    # Die SW-Dachmarkisen auch bei Südsonne herunterfahren, da es sonst dort sehr warm wird.
+    ensure_state('cover.f3_cov_dachfenster_gross_aussen_cover', sunprot_southface_is_active or sunprot_westface_is_active)
+    ensure_state('cover.f3_cov_bad_cover',                      sunprot_southface_is_active or sunprot_westface_is_active)
